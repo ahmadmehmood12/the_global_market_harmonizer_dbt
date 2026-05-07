1e1b84dd76e9160de86b79ea8bff43f0d4c920f1
Ahmad Mehmood <ahmad.mehmood@vionlogics.com>
2026-05-07 19:41:32 +0500

    change in the file

--- the_harmonizer_dbt/models/gold/gold_internal_orders.sql
+++ the_harmonizer_dbt/models/gold/gold_internal_orders.sql
@@ -2,6 +2,6 @@ SELECT
    *
 FROM {{ source('source','dim_date') }}
 
-
+token = "3255435123423432"
 {# we can also write like this but best practice is to get it from source using jinja function  #}
 {# select * from dbt_tutorial_dev.source.dim_date #}
